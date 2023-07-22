1639bf3aeeee660bfa9a5294d143a0c97e90b0a5
Jose Jaime Bisuña <jaimebisona@gmail.com>
2023-07-23 07:59:25 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,12 @@ import { CodeRecords } from './components/CodeRecords';
 import './App.css';
 import React from 'react';
 
-const App = () => {
-    const count = useSelector((state: RootState) => state.counter.counter);
-
-    const dispatch = useDispatch<AppDispatch>();
-
-    return (
-        <>
-            <CodeRecords />
-            <br />
-            <InputField />
-        </>
-    );
-};
+const App = () => (
+    <>
+        <CodeRecords />
+        <br />
+        <InputField />
+    </>
+);
 
 export default App;
